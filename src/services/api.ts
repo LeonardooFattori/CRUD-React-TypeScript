e74b8f022ee367ae471d959047ae55e4880b41af
Leonardo Moura <leonardo.royale111@gmail.com>
2025-04-29 13:26:04 -0300

    Last version CRUD

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://json-server-separado-production.up.railway.app',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
